4ed66b8d6b5e5eaca50ce50d85358ab890bc7cea
JaviGomez02 <javigomez055@gmail.com>
2023-02-22 13:42:14 +0100

    Añadir categoria

--- src/app/categorias/add-categoria/add-categoria.component.ts
+++ src/app/categorias/add-categoria/add-categoria.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Categoria } from '../../interfaces/categoria.interface';
+import { categoriaService } from '../../services/categoria.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-categoria',
@@ -11,7 +14,7 @@ export class AddCategoriaComponent {
 
   nuevaCategoria:Categoria={nombre:"", descripcion: "", id: 0}
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private categoriaService:categoriaService, private route:Router) { }
 
   myForm: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.minLength(3)]],
@@ -21,6 +24,36 @@ export class AddCategoriaComponent {
   isValidField(field: string){
     return this.myForm.controls[field].errors
     && this.myForm.controls[field].touched
-}
+  }
+
+
+  addCategoria(){
+    if (this.myForm.invalid){ //Para que no se pueda mandar el formulario sin tocar los campos
+      this.myForm.markAllAsTouched() 
+    }
+    else{
+      this.categoriaService.addCategoria(this.myForm.value.nombre, this.myForm.value.descripcion)
+      .subscribe({
+        next:(resp)=>{
+          if(resp){
+            Swal.fire({
+              icon: 'success',
+              title: 'Categoria añadida correctamente',
+              text: 'Nombre: '+this.myForm.value.nombre+', descripcion: '+this.myForm.value.descripcion
+            })
+            this.route.navigate(["/categoria"])
+          }
+          else{
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'No se pudo añadir la categoria'
+            })
+          }
+        }
+      })
+    }
+    
+  }
 
 }
--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, switchMap, catchError, of } from 'rxjs';
 import { Categoria } from '../interfaces/categoria.interface';
 
 @Injectable({
@@ -16,6 +16,17 @@ export class categoriaService{
     getCategorias():Observable<Categoria[]>{
       return this.http.get<Categoria[]>(this.url)
     }
+
+    addCategoria(nombre:string, descripcion:string):Observable<boolean>{
+      return this.http.post<any>(this.url, {"nombre":nombre, "descripcion":descripcion})
+      .pipe( switchMap(resp => {
+        return of(true);
+      }),catchError(error => {
+          return of(false);
+      })
+      )
+    }
+    
 
 
 }
