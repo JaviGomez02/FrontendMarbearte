6561761a27657ba30ab5efac2d952eb81bbdef3c
JaviGomez02 <javigomez055@gmail.com>
2023-02-24 11:57:52 +0100

    Poco cambio

--- src/app/products/all-products/all-products.component.ts
+++ src/app/products/all-products/all-products.component.ts
@@ -4,6 +4,7 @@ import { Product } from '../../interfaces/product.interface';
 import { Content } from '../../interfaces/page.interface';
 import { CookieService } from 'ngx-cookie-service';
 import { authService } from '../../auth/auth.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-all-products',
@@ -12,7 +13,7 @@ import { authService } from '../../auth/auth.service';
 })
 export class AllProductsComponent implements OnInit {
 
-  constructor(private servicio:productService, private cookieService:CookieService, private authService: authService) { }
+  constructor(private servicio:productService, private cookieService:CookieService, private authService: authService, private route: ActivatedRoute) { }
 
   isAdmin:boolean=false
 
@@ -29,9 +30,8 @@ export class AllProductsComponent implements OnInit {
   pageNumber:number=1
 
   ngOnInit(): void {
-
-    this.servicio.getProducts(this.pageNumber, this.sizeNumber)
-    .subscribe({
+      this.servicio.getProducts(this.pageNumber, this.sizeNumber)
+      .subscribe({
       next: (resp)=>{
         this.lista=resp.content
         this.totalPages=resp.totalPages
@@ -44,6 +44,8 @@ export class AllProductsComponent implements OnInit {
         
       }
     })
+    
+    
 
     this.token = this.cookieService.get('token')
     if(this.token && this.authService.decodeJwt(this.token).role=='ADMIN'){
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -12,6 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AllProductsComponent,
     DatatableProductComponent
+    
   ],
   imports: [
     CommonModule,
