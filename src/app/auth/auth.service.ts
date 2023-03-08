ed25afa31ba9502a92105b1e9b2778fcf5145f27
JaviGomez02 <javigomez055@gmail.com>
2023-03-08 21:52:27 +0100

    Preparado para desplegar

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -10,11 +10,11 @@ import { DecodeToken } from '../interfaces/decode-token.interface';
 
 @Injectable({
     providedIn: 'root'
-  })
+})
 
-export class authService{
+export class authService {
 
-    url:string = 'https://apimarbearte-production.up.railway.app/auth'
+    url: string = 'https://apimarbearte-production.up.railway.app/auth'
     httpOptions = {
         headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     };
@@ -23,69 +23,69 @@ export class authService{
         headers: new HttpHeaders({ 'Authorization': 'Bearer eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJKYXZpR29tZXoiLCJleHAiOjE2ODA1NDE4ODYsInJvbGUiOiJBRE1JTiIsImVtYWlsIjoiamF2aWdvbWV6MDU1QGdtYWlsLmNvbSJ9.45wSZ4qbKcpebGWll9AFGfdEw_McLqu9Ko6WexmHaHsGpeOa-LGu_BB4UjjLs5lk' })
     }
 
-    constructor(private http: HttpClient, private cookieService:CookieService){
+    constructor(private http: HttpClient, private cookieService: CookieService) {
         this.http.get('https://apimarbearte-production.up.railway.app/jwt')
-        .subscribe({
-             next:()=>this.loged.next(true),
-             error:()=>this.loged.next(false)
-         })
+            .subscribe({
+                next: () => this.loged.next(true),
+                error: () => this.loged.next(false)
+            })
 
         this.http.get('https://apimarbearte-production.up.railway.app/jwtAdmin')
-        .subscribe({
-             next:()=>this.admin.next(true),
-              error:()=>this.admin.next(false)
-         })
+            .subscribe({
+                next: () => this.admin.next(true),
+                error: () => this.admin.next(false)
+            })
     }
 
-    private admin = new BehaviorSubject<boolean> (false);
+    private admin = new BehaviorSubject<boolean>(false);
 
-    private loged = new BehaviorSubject<boolean> (false);
+    private loged = new BehaviorSubject<boolean>(false);
 
     get isAdmin() {
         return this.admin.asObservable();
-      }
+    }
 
-    get isLoged(){
+    get isLoged() {
         return this.loged.asObservable();
     }
 
-    register(username: string, email:string, contrasena:string, nombre:string):Observable<boolean>{
-        return this.http.post<any>(this.url+"/register", {"username":username, "contrasena":contrasena, "nombre":nombre, "email":email},this.httpOptions)
-        .pipe( switchMap(resp => {
+    register(username: string, email: string, contrasena: string, nombre: string): Observable<boolean> {
+        return this.http.post<any>(this.url + "/register", { "username": username, "contrasena": contrasena, "nombre": nombre, "email": email }, this.httpOptions)
+            .pipe(switchMap(resp => {
                 return of(true);
-            }),catchError(error => {
+            }), catchError(error => {
                 return of(false);
             })
-        )
+            )
     }
 
-    verify(code: string, username: string):Observable<boolean>{
-        return this.http.get<any>('https://apimarbearte-production.up.railway.app/verify?code='+code+'&username='+username, this.httpOptions)
-        .pipe( switchMap(resp => {
+    verify(code: string, username: string): Observable<boolean> {
+        return this.http.get<any>('https://apimarbearte-production.up.railway.app/verify?code=' + code + '&username=' + username, this.httpOptions)
+            .pipe(switchMap(resp => {
                 return of(true);
-            }),catchError(error => {
+            }), catchError(error => {
                 return of(false);
             })
-        )
+            )
     }
 
-    
-    login(username: string, password: string):Observable<boolean>{
+
+    login(username: string, password: string): Observable<boolean> {
         // console.log(username)
         // console.log(password)
-        return this.http.post<AuthResponse>(this.url+"/login", {username, password},this.httpOptions)
-        .pipe( switchMap(token => {
+        return this.http.post<AuthResponse>(this.url + "/login", { username, password }, this.httpOptions)
+            .pipe(switchMap(token => {
                 this.cookieService.set('token', token.token)
-                if(this.decodeJwt(token.token).role=='ADMIN'){
+                if (this.decodeJwt(token.token).role == 'ADMIN') {
                     this.admin.next(true)
                 }
                 this.loged.next(true)
                 return of(true);
-            }),catchError(error => {
+            }), catchError(error => {
                 this.cookieService.delete('token');
                 return of(false);
             })
-        )
+            )
     }
 
     logout() {
@@ -94,28 +94,27 @@ export class authService{
         this.loged.next(false)
     }
 
-    isLoggedIn(){
-        if(this.cookieService.get('token')){
+    isLoggedIn() {
+        if (this.cookieService.get('token')) {
             return true
         }
-        else{
+        else {
             return false
         }
     }
 
-    isAdminGuard(){
-        let token=this.cookieService.get('token')
-        if(token){
-            let rol=this.decodeJwt(token).role
-            if(rol=='ADMIN'){
+    isAdminGuard() {
+        let token = this.cookieService.get('token')
+        if (token) {
+            let rol = this.decodeJwt(token).role
+            if (rol == 'ADMIN') {
                 return true
             }
         }
-            return false
-        
+        return false
     }
 
-    decodeJwt(jwt: string): DecodeToken{
+    decodeJwt(jwt: string): DecodeToken {
         return jwt_decode(jwt)
     }
 
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Categoria } from 'src/app/interfaces/categoria.interface';
+import { categoriaService } from 'src/app/services/categoria.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private categoriaService:categoriaService, private route: Router) { }
+
+  listaCategorias:Categoria[]=[]
 
   ngOnInit(): void {
+    this.categoriaService.getCategorias()
+    .subscribe({
+      next: (resp)=>{
+        for(let i=0; i<3; i++){
+          this.listaCategorias.push(resp[i])
+        }
+      }
+    })
   }
+
+  verProductos(id:number){
+    this.route.navigateByUrl('products/all?idCategoria='+id)
+  }
+
+
 
 }
