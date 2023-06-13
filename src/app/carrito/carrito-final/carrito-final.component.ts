ec86ed47e17140929237d92b8e939f814ea62939
JaviGomez02 <javigomez055@gmail.com>
2023-06-13 15:50:20 +0200

    Paypal implementado. Faltan arreglar validaciones

--- src/app/carrito/carrito-final/carrito-final.component.ts
+++ src/app/carrito/carrito-final/carrito-final.component.ts
@@ -51,6 +51,7 @@ export class CarritoFinalComponent implements OnInit {
   }
 
   private initConfig(): void {
+
     this.payPalConfig = {
       currency: 'EUR',
       clientId: 'AaQ6uY9Tnzc99eBuzOXnYkqWwxE26o7AAOn-TjoFlVmEwC4aOq0ACAA0vSah2vt8cYFmpOaDD6XqDsjW',
@@ -59,23 +60,15 @@ export class CarritoFinalComponent implements OnInit {
         purchase_units: [{
           amount: {
             currency_code: 'EUR',
-            value: '9.99',
+            value: this.calcularTotal().toString(),
             breakdown: {
               item_total: {
                 currency_code: 'EUR',
-                value: '9.99'
+                value: this.calcularTotal().toString()
               }
             }
           },
-          items: [{
-            name: 'Enterprise Subscription',
-            quantity: '1',
-            category: 'DIGITAL_GOODS',
-            unit_amount: {
-              currency_code: 'EUR',
-              value: '9.99',
-            },
-          }]
+          items: this.listaItems()
         }]
       },
       advanced: {
@@ -86,25 +79,48 @@ export class CarritoFinalComponent implements OnInit {
         layout: 'vertical'
       },
       onApprove: (data, actions) => {
-        console.log('onApprove - transaction was approved, but not authorized', data, actions);
-        actions.order.get().then((details: any) => {
-          console.log('onApprove - you can get full order details inside onApprove: ', details);
-        });
+        // console.log('onApprove - transaction was approved, but not authorized', data, actions);
+        // actions.order.get().then((details: any) => {
+        //   console.log('onApprove - you can get full order details inside onApprove: ', details);
+        // });
 
       },
       onClientAuthorization: (data) => {
         console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+        this.comprar()
       },
       onCancel: (data, actions) => {
-        console.log('OnCancel', data, actions);
+        // console.log('OnCancel', data, actions);
       },
       onError: err => {
         console.log('OnError', err);
       },
       onClick: (data, actions) => {
-        console.log('onClick', data, actions);
+        // console.log('onClick', data, actions);
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Algo ha ido mal'
+        })
       }
     };
+  }
+
+  listaItems() {
+    let items: any[] = []
+    let item = {}
+    for (let i = 0; i < this.listaProductos.length; i++) {
+      item = {
+        name: this.listaProductos[i].producto.nombre,
+        quantity: this.listaProductos[i].cantidad.toString(),
+        unit_amount: {
+          currency_code: 'EUR',
+          value: this.listaProductos[i].producto.price.toString()
+        }
+      }
+      items.push(item)
+    }
+    return items;
   }
 
   obtenerDirecciones() {
